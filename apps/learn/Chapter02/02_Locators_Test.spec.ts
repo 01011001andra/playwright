c3ddf15b41188038ececefb908404030b6de01b6
01011001andra <yandrayt1@gmail.com>
2026-03-11 16:30:45 +0700

    update

--- apps/learn/Chapter02/02_Locators_Test.spec.ts
+++ apps/learn/Chapter02/02_Locators_Test.spec.ts
@@ -4,9 +4,29 @@ import { test, expect } from "@playwright/test";
 // Write a test
 test("Locators in Playwright", async ({ page }) => {
   // Go to URL
-  await page.goto("http://youtube.com/");
+  await page.goto('https://github.com/');
 
   // getByRole
-  await page.getByRole('link', {name:''})
- 
+  await page.getByRole('link', { name: 'Sign in' }).click();
+
+  // getByLabel
+  await page.getByLabel('Username or email address').fill('yandrayt1@gmail.com');
+
+  // getByPlaceholder
+  await page.getByPlaceholder('Password').fill('123123123');
+
+  // getByText
+  await page.getByText('Sign in').click();
+
+  // getByAltText
+  await page.getByAltText('Avatar').click();
+
+  // getByTitle
+  await page.getByTitle('Notifications').click();
+
+  // getByTestId
+  await page.getByTestId('test-id').click();
+
+
+
 });
